cc6b4945a6db5287566fb5646bdf34549d35c2e1
emuch <57587195+nik48@users.noreply.github.com>
2023-01-10 20:46:42 +0100

    Ścieżka względna i poprawa czytelności

--- src/emuch/efs/Music.java
+++ src/emuch/efs/Music.java
@@ -1,31 +1,31 @@
 package emuch.efs;
 
 import java.io.File;
+import java.io.IOException;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
 
-class Music{
-    static void playMusic() {
-        File muzyka = new File("C:/Users/Emus/Desktop/Programy Studia/JAVA JDK/Empirefromscratch/sounds/World of Knights.wav");
+class Music extends Thread{
+    Menu menu;
+    boolean playing = true;
+    String soundpath;
+
+    void playMusic() throws IOException {
+        this.soundpath = new File("sounds").getCanonicalPath().toString();
+        File muzyka = new File(this.soundpath + "/World of Knights.wav");
         playSound(muzyka);
     }
     
-    static void playSound(File Sound) {
+    void playSound(File Sound) {
         try {
             Clip clip = AudioSystem.getClip();
             clip.open(AudioSystem.getAudioInputStream(Sound));
-            FloatControl gainControl = 
-            (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
             gainControl.setValue(-20);
-            clip.start();
             clip.loop(Clip.LOOP_CONTINUOUSLY);
         }
         catch(Exception e){
         }
     }
 }
-
-//To Do:
-//wyłaczenie muzyki przez kilknięcie "X".
-//ściszenie muzyki o 75% - 6dB                  //DONE
