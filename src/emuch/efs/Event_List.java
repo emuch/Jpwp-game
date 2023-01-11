45db504f4ac8ac9310a5e7b5e67eb9fcac980937
emuch <57587195+nik48@users.noreply.github.com>
2023-01-11 13:51:11 +0100

    Jeszcze więcej eventów

--- src/emuch/efs/Event_List.java
+++ src/emuch/efs/Event_List.java
@@ -8,15 +8,19 @@ class Event_List {
     Event_List(Menu menu) {
         list = new ArrayList<Event>();  //od najmniejszego do największego prawdopodobieństwa  //najpierw ciche eventy a potem głośne od najmniejszego
         list.add(new NewResources(menu)); //dodawanie zasobów
-        list.add(new Epidemic(menu));//0.05
-        list.add(new DeadOxMine(menu));//0.1
+        list.add(new ThiefOnStreets(menu));//0.05
+        list.add(new Epidemic(menu));//0.05 , 8 week
+        list.add(new RiversFlooded(menu));//0.1, 16 week
+        list.add(new StrikeSawmill(menu));//0.1
+        list.add(new StrikeStonemine(menu));//0.1
+        list.add(new StrikeGoldmine(menu));//0.1
+        list.add(new DeadOxMine(menu));//0.1 , 2 week
+        list.add(new BanditsStonemine(menu));//0.2 12 week
         list.add(new SawmillSaw(menu));//0.2
+        list.add(new ThiefsInMagazine(menu));//0.2 , 3 week
         list.add(new BurningForest(menu));//0.3
         list.add(new DrainedStoneMine(menu));//0.3
         list.add(new GoldMineCollapse(menu));//0.3
-        //list.add(new (menu));
-        //list.add(new (menu));
-        //list.add(new (menu));
     }
 }
 class NewResources extends Event{
@@ -104,7 +108,7 @@ class SawmillSaw extends Event{   //głośny event
     void run(){//ilość zasobów
         this.menu.buildingResources.wood-=10;
         JFrame okno = new JFrame();
-        JOptionPane.showMessageDialog(okno, "Piły, z których korzystaliśmy do tej pory w tartaku, nie tną już tak jak dawniej,\n chyba nadeszła pora wymiany na nowe, to spowoduje przestoje w pracy.\n Tracisz 10 drewna.", "Zepsuta piły.", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(okno, "Piły, z których korzystaliśmy do tej pory w tartaku, nie tną już tak jak dawniej,\n chyba nadeszła pora wymiany na nowe, to spowoduje przestoje w pracy.\n Tracisz 10 drewna.", "Zepsute piły.", JOptionPane.INFORMATION_MESSAGE);
     }
 }
 
@@ -147,5 +151,135 @@ class Epidemic extends Event{   //głośny event
         this.menu.buildingResources.goldGain-=10;
         JFrame okno = new JFrame();
         JOptionPane.showMessageDialog(okno, "W naszym zamku wybuchła epidemia będzie to miało znaczne skutki i wpływ na dostawy.\n Przede wszystkim musimy zadbać o lekarzy i leczenie populacji, a to będzie kosztowne.\n Musimy być również świadomi problemu z brakiem ludzi do pracy i w konsekwencji strat na rzecz zasobów.\n Tracisz 40 złota, 20 drewna, 20 kamienia i po 10 przyrostu każdego z zasobów.", "Epidemia!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class ThiefsInMagazine extends Event{   //głośny event
+    ThiefsInMagazine(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.2 && this.menu.rcounter.returnWeek() > 3)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.wood-=30;
+        this.menu.buildingResources.stone-=30;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Odkryliśmy, że z naszego magazynu zostały skradzione zasoby,\n to musiała być zorganizowana akcja,\n bo straciliśmy naprawdę dużo ciężkich materiałów.\n Tracisz 30 drewna i 30 kamienia.", "Złodzieje w magazynie!", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class ThiefOnStreets extends Event{   //głośny event
+    ThiefOnStreets(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.05)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.gold-=40;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Niedawno mieszkańcy zaczęli zgłaszać informacje o sporej ilości kradzieży na ulicach,\n jest to zapewne wina kieszonkowców czyhających na targach.\n Poddani mogą mieć przez to problem z płaceniem podatków,\n musimy zadbać o ich bezpieczeństwo albo zmniejszyć podatki.\n Tracisz 40 złota.", "Kieszonkowcy grasują wszędzie.", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class StrikeSawmill extends Event{   //głośny event
+    StrikeSawmill(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.1)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.wood-=10;
+        this.menu.buildingResources.gold-=10;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Ludzie są oburzeni panującymi w tartaku\n warunkami pracy, uważają to za wyzysk.\n Spowoduje to przestoje w produkcji, musimy podnieść\n im pensje, żeby zapobiec dalszym strajkom.\n Tracisz 10 drewna i 10 złota.", "Strajk w tartaku.", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class StrikeStonemine extends Event{   //głośny event
+    StrikeStonemine(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.1)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.stone-=10;
+        this.menu.buildingResources.gold-=10;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Ludzie są oburzeni panującymi w kamieniołomie\n warunkami, uważają to za niehumanitarne.\n Spowoduje to przestoje w produkcji, musimy podnieść\n im pensje, żeby zapobiec dalszym strajkom.\n Tracisz 10 kamienia i 10 złota.", "Strajk w kamieniołomie.", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class StrikeGoldmine extends Event{   //głośny event
+    StrikeGoldmine(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.1)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.gold-=20;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Ludzie są oburzeni panującymi w kopalni złota\n warunkami pracy, uważają to za wyzysk.\n Spowoduje to przestoje w wydobyciu, musimy podnieść\n im pensje, żeby zapobiec dalszym strajkom.\n Tracisz 20 złota.", "Strajk w kopalni złota.", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class BanditsStonemine extends Event{   //głośny event
+    BanditsStonemine(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.2 && this.menu.rcounter.returnWeek() > 12)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.stone-=30;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Na nasze tereny z pobliskich gór przybyli bandyci i zajęli kamieniołom, będziemy musieli go odbić albo poszukać nowego złoża. Tracisz 30 kamienia.", "Bandyci z pobliskich gór.", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
+
+class RiversFlooded extends Event{   //głośny event
+    RiversFlooded(Menu menu) {
+        super(menu);
+    }
+    @Override
+    boolean check_conditions(){
+        if(Math.random() < 0.1 && this.menu.rcounter.returnWeek() > 16)
+            return true;
+        return false;
+    }
+    @Override
+    void run(){//ilość zasobów
+        this.menu.buildingResources.wood-=40;
+        this.menu.buildingResources.stone-=40;
+        JFrame okno = new JFrame();
+        JOptionPane.showMessageDialog(okno, "Ostatnie obfite opady deszczu spowodowały\n wylew rzek zarówno tych w lasach, jak i górach.\n Cały kamieniołom został zalany, a tartak nie ma dostępu do materiałów. Musimy poczekać na suszę albo liczyć na ciepłe słońce.\n Tracisz 40 drewna i 40 kamienia.", "Obifte opady.", JOptionPane.INFORMATION_MESSAGE);
     }
 }
